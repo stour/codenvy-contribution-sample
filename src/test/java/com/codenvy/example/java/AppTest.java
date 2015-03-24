70818fa73ff715bdbcf3d5a78040d1f605075b03
Stéphane Tournié <stephane.tournie@serli.com>
2015-03-24 18:10:49 +0100

    * Unit test for simple App.

--- src/test/java/com/codenvy/example/java/AppTest.java
+++ src/test/java/com/codenvy/example/java/AppTest.java
@@ -6,6 +6,8 @@ import junit.framework.TestSuite;
 
 /**
  * Unit test for simple App.
+ * Unit test for simple App.
+ * Unit test for simple App.
  */
 public class AppTest
         extends TestCase {
